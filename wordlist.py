59180080b9d1cd8821266ce86e424486a2826d8c
jayantmukundam <62736847+jayantmukundam@users.noreply.github.com>
2021-02-17 20:58:45 +0530

    Add files via upload

---
+++ wordlist.py
@@ -0,0 +1,13 @@
+def ramayanWords():
+    wordlist1=["URMILA","MAKARDHWAJA","MEGHNAD","SITA","KUBER","KUSH"]
+    return wordlist1
+def ramayanHints():
+    hint=["LAXMAN'S WIFE","HANUMAN'S SON","INDRAJIT","MAITHILI","RAVAN'S HALF-BROTHER","RAM'S ELDER SON"]
+    return hint
+def mahabharatWords():
+    wordlist2=["DUSHALA","BHISHMA","GANGA","KARNA","UTHARA","SHIKANDINI"]
+    return wordlist2
+def mahabharatHints():
+    hint=["DHRITRASHTRA'S DAUGHTER","DEVVRAT","SHANTANU'S 1ST WIFE","KUNTI'S 1ST SON","ABHIMANYU'S WIFE","DRUPAD'S 1ST DAUGHTER"]
+    return hint
+    
